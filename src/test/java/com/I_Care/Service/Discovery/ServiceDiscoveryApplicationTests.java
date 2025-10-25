7582bf18d064e7c08924854be411e545669e2b00
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2025-10-25 19:22:40 +0530

    Intial Setup

---
+++ src/test/java/com/I_Care/Service/Discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.I_Care.Service.Discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
